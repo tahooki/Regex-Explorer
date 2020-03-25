0f7b10737c6e66d61520ab167d921c86740a78ea
Tahooki <tahooki12@gmail.com>
2020-03-25 20:34:10 +0900

    remove backend, add firebase

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Regex-Explorer
 정규식 검색하는 사이트입니다.
+
+## Functions
+- StoryBook : https://storybook.js.org/
+`npx -p @storybook/cli sb init`
+https://www.learnstorybook.com/intro-to-storybook/angular/en/test/
+`storybook-chromatic --dev` 이건 유료...
+스토리북을 사용하는 이유 : 컴포넌트를 각각의 상태를 만들어 컴포넌트를 개발하기 편하기 때문.
+스토리북으로 UI 테스트를 할 수 있는 방법이 있는데... 학습중이다.
+  - https://applitools.com/
+  - https://percy.io/
+
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'front'`, () => {
+  it(`should have as title 'regex-explorer'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('front');
+    expect(app.title).toEqual('regex-explorer');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('front app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('regex-explorer app is running!');
   });
 });
