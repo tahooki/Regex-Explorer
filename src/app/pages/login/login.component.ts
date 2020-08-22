1188258967460f0c1f69bcc9dcf661943cd6811d
Tahooki <tahooki12@gmail.com>
2020-08-22 23:38:10 +0900

    fix firebase ui (login)

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,31 +1,156 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { Router } from '@angular/router';
-import { FirebaseUISignInFailure, FirebaseUISignInSuccessWithAuthResult } from 'firebaseui-angular';
+import * as firebaseui from 'firebaseui';
+import * as firebase from 'firebase';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
-  selector: 'app-login',
+  selector:    'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls:   [ './login.component.scss' ]
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private afAuth: AngularFireAuth, private router: Router) {
+  user: any;
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private firestore: AngularFirestore
+  ) {
   }
 
   ngOnInit(): void {
-    this.afAuth.authState.subscribe(d => console.log(d));
+    this.afAuth.authState.subscribe(user => { // 아래꺼랑 동일.
+      console.log('authState', user);
+      this.user = user;
+
+      if (user) {
+        this.firestore.collection('user').doc(user.uid).get().subscribe(data => {
+          console.log('get data ', data.data());
+        });
+      } else {
+        this.makeLoginContainer();
+      }
+    });
+
+    // this.afAuth.user.subscribe(user => {
+    //   console.log('user', user);
+    //
+    //   if(user) {
+    //     this.firestore.collection('user').doc(user.uid).get().subscribe(data => {
+    //       console.log('get data ', data.data());
+    //     })
+    //   }
+    // });
+
+  }
+
+  makeLoginContainer(): void {
+    const ui       = new firebaseui.auth.AuthUI(firebase.auth());
+    const uiConfig = this.getUiConfig();
+    ui.start('#firebaseui-auth-container', uiConfig);
+
+    // Is there an email link sign-in?
+    if (ui.isPendingRedirect()) {
+      ui.start('#firebaseui-auth-container', uiConfig);
+    }
+// This can also be done via:
+    if (firebase.auth().isSignInWithEmailLink(window.location.href)) {
+      ui.start('#firebaseui-auth-container', uiConfig);
+    }
   }
 
   logout() {
     this.afAuth.signOut();
   }
 
-  successCallback(data: FirebaseUISignInSuccessWithAuthResult) {
-    console.log('successCallback', data);
-  }
+  private getUiConfig(): any {
+    const uiConfig = {
+      callbacks:        {
+        signInSuccessWithAuthResult: (authResult, redirectUrl) => {
+          console.log('authResult', authResult);
+          console.log('authResult', authResult.user);
+          console.log('authResult', authResult.user.uid);
+          console.log('authResult', authResult.user.email);
+          console.log('redirectUrl', redirectUrl);
+          const user = this.firestore.collection('user').doc(authResult.user.uid).get();
+          console.log('user', user);
+          // 이곳에서 user 도큐먼트에 값을 넣으면 될거같음 !
+          if (!user) {
+            this.firestore.collection('user').doc(authResult.user.uid).set({
+              email: authResult.user.email,
+              image: authResult.user.photoURL,
+              name:  authResult.user.displayName
+            }).then(() => {
+              alert('??????????');
+            });
+          }
+          /*
+          *
+          uid: "oCsKnXkui8fC6Bl3DV5RCGf54U13"
+displayName: "Tahooki Developer"
+photoURL: "https://lh5.googleusercontent.com/-sxdtiLzYESs/AAAAAAAAAAI/AAAAAAAAAAA/AAKWJJPIS6oGu9SVBiH93BTENCLM1MAh5g/photo.jpg"
+email: "tahooki12@gmail.com"
+emailVerified: true
+phoneNumber: null
+          * */
+          // User successfully signed in.
+          // Return type determines whether we continue the redirect automatically
+          // or whether we leave that to developer to handle.
+          return false;
+        },
+        uiShown() {
+          console.log('ho!!!!');
+          // The widget is rendered.
+          // Hide the loader.
+          // document.getElementById('loader').style.display = 'none';
+        }
+      },
+      // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+      signInFlow:       'popup',
+      signInSuccessUrl: 'http://localhost:4200/login',
+      // signInOptions: [
+      //   // Leave the lines as is for the providers you want to offer your users.
+      //   firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      //   firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      //   firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      //   firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      //   firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      //   firebase.auth.PhoneAuthProvider.PROVIDER_ID
+      // ],
+      signInOptions:    [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        {
+          scopes:           [
+            'public_profile',
+            'email',
+            'user_likes',
+            'user_friends'
+          ],
+          customParameters: {
+            auth_type: 'reauthenticate'
+          },
+          provider:         firebase.auth.FacebookAuthProvider.PROVIDER_ID
+        },
+        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        {
+          requireDisplayName: false,
+          provider:           firebase.auth.EmailAuthProvider.PROVIDER_ID
+        },
+        firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+        firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+      ],
+      // Terms of service url.
+      tosUrl:           '<your-tos-url>',
+      // Privacy policy url.
+      privacyPolicyUrl: '<your-privacy-policy-url>',
+      // tosUrl:           '<your-tos-link>',
+      // privacyPolicyUrl: '<your-privacyPolicyUrl-link>',
+      credentialHelper: firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM
+      // Other config options...
+    };
 
-  errorCallback(data: FirebaseUISignInFailure) {
-    console.warn('errorCallback', data);
+    return uiConfig;
   }
 }
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -4,45 +4,9 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
-// currently there is a bug while building the app with --prod
-// - https://github.com/RaphaelJenni/FirebaseUI-Angular/issues/76
-// the plugin exposes the two libraries as well. You can use those:
-import { FirebaseUIModule, firebase, firebaseui } from 'firebaseui-angular';
-
-
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from '../../../environments/environment';
-
-const firebaseUiAuthConfig: firebaseui.auth.Config = {
-  signInFlow:       'popup',
-  signInOptions:    [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    {
-      scopes:           [
-        'public_profile',
-        'email',
-        'user_likes',
-        'user_friends'
-      ],
-      customParameters: {
-        auth_type: 'reauthenticate'
-      },
-      provider:         firebase.auth.FacebookAuthProvider.PROVIDER_ID
-    },
-    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-    firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    {
-      requireDisplayName: false,
-      provider:           firebase.auth.EmailAuthProvider.PROVIDER_ID
-    },
-    firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-    firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
-  ],
-  // tosUrl:           '<your-tos-link>',
-  // privacyPolicyUrl: '<your-privacyPolicyUrl-link>',
-  credentialHelper: firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM
-};
 
 
 @NgModule({
@@ -51,7 +15,6 @@ const firebaseUiAuthConfig: firebaseui.auth.Config = {
     CommonModule,
     LoginRoutingModule,
     AngularFireAuthModule,
-    FirebaseUIModule.forRoot(firebaseUiAuthConfig),
     AngularFireModule.initializeApp(environment.firebase)
   ]
 })
